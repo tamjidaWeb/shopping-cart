c06a4ac6d85b6be2874cdbd23c795b039472c9b8
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-01-04 16:23:24 +0600

    c

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function updateProgress(progress) {
-    const progressRing = document.getElementById('progress-ring');
-    const progressText = document.getElementById('progress-text');
+const buttons = document.querySelectorAll('.button');
+const body=document.querySelector('body');
 
-    // Calculate the stroke-dashoffset based on the progress
-    const circumference = 314; // Circumference of the circle (2 * π * radius)
-    const offset = circumference - (progress / 100) * circumference;
-    
-    // Update the progress ring and text
-    progressRing.style.strokeDashoffset = offset;
-    progressText.textContent = `${progress}%`;
-  }
+buttons.forEach(function(button){
+  button.addEventListener('click', function(e){
+    console.log(e);
+    console.log(e.target);
 
-  // Set initial progress (e.g., 70% or 80%)
-  let progress = 70;  // Change this to 70 or 80 or any value between 0-100
-  updateProgress(progress);  
-
+    if(e.target.id==='gray'){
+      body.style.backgroundColor = e.target.id;
+    }
+  })
+})
