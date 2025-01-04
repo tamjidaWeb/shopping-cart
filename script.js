3a12f1b8ab3934b6f409df2a9c8d4e01517c300a
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-01-04 20:26:29 +0600

    c

--- script.js
+++ script.js
@@ -11,3 +11,85 @@ buttons.forEach(function(button){
     }
   })
 })
+
+
+
+// const form=document.querySelector('form');
+// form.addEventListener('submit',function(e){
+//   e.preventDefault();
+
+//   const height=parseInt(document.querySelector("#height").value)
+//   const weight=parseInt(document.querySelector("#weight").value);
+//   const results=document.querySelector("#results");
+
+//   if(height === '' || height<0 || isNaN(height)){
+//     results.innerHTML= `please give a valid height ${height}`
+//   }
+//   if(weight === '' || weight<0 || isNaN(weight)){
+//     results.innerHTML= `please give a valid weight ${weight}`
+//   }
+
+//   else{
+//     const bmi = (weight/ ((height*height)/10000)).toFixed(2);
+
+//     results.innerHTML = `Here is your BMI result ${bmi}`
+//   }
+// })
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  constresults = document.querySelector('#results');
+
+  if(height=== '' || height<0 || isNaN(height)){
+    results.innerHTML = `please enter a valid numberrrrrrrrrrr${height}`;
+  }
+  if(weight=== '' || weight<0 || isNaN(weight)){
+    results.innerHTML = `please enter a valid numberrrrrrrrrrr ${weight}`;
+  }
+
+  else{
+    const bmi = (weight/ ((height*height)/10000)).toFixed(2);
+    results.innerHTML= `here is the bmi ${bmi}`;
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
